061097d9694f36749ee21b11f20a8b42d43489fe
Anand290793 <anandyadav654@outlook.com>
2018-05-23 23:16:54 +0530

    Tour_Of_Heroes

---
+++ Angular6_TourOfHeroes/avengers/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Scarlet Witch' },
+  { id: 12, name: 'Hawkeye' },
+  { id: 13, name: 'Vision' },
+  { id: 14, name: 'Hulk' },
+  { id: 15, name: 'Thor' },
+  { id: 16, name: 'Dr.Strange' },
+  { id: 17, name: 'Groot' },
+  { id: 18, name: 'Captain' },
+  { id: 19, name: 'RedWing' },
+  { id: 20, name: 'BlackWidow' }
+];
